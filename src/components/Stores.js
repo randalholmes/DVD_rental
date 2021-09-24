3d052b21357642e6a16b48d4b9f2fdbb50992bb4
Randal Holmes <randal.holmes@gmail.com>
2021-09-24 11:10:46 -0700

    Removed some Redux stuff that was not needed.

--- src/components/Stores.js
+++ src/components/Stores.js
@@ -1,18 +1,11 @@
 
 import StoreList from './StoreList'
 import Customers from './Customers'
-// import { useDispatch } from 'react-redux'
-// import { bindActionCreators } from 'redux'
-// import * as actionCreators from '../state/actionCreators/actionCreators'
 
 
 import './css/stores.css'
 
 const Stores = () => {
-    // const dispatch = useDispatch()
-    // const { selectStore, changeCustomerList } = bindActionCreators(actionCreators, dispatch)
-
-    // console.log("action creators: ", actionCreators)
 
     return (
 
