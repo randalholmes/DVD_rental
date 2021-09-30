90b9ae3c484cba7bcb95f9eac948da15bc8ad9ce
Randal Holmes <randal.holmes@gmail.com>
2021-09-30 16:27:47 -0700

    Fixed  bunch of issues with form inputs.

--- src/components/EditCustomer.js
+++ src/components/EditCustomer.js
@@ -2,58 +2,107 @@
 
 import './css/editCustomer.css'
 
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { setCustomer } from '../state/actionCreators/actionCreators'
+
 const EditCustomer = () => {
+    const { customer: cus, curCustomerId: id} = useSelector(state => state)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        id ? console.log("id changed") : console.log("Id null")
+        
+    }, [id, dispatch])
+
+
+
+    const updateCustomer = async () => {
+        const formData = Array.from(document.getElementsByClassName('input'))
+            .map( elm => [elm.getAttribute('name'), elm.innerText])
+
+        console.log(formData)
+
+        const changedVals = formData.filter( ([name, value]) => value !== cus[name])
+        console.log("changed values: ", changedVals)
+
+        // If no customer data has changed, then don't make call to server.
+        if (changedVals.length === 0) {
+            return
+        }
+
+        // Create data object to send to server
+        const newData = {customer_id: id, changedVals}
+
+        console.log("newData", newData)
+
+
+
+
+
+    }
+
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        updateCustomer()
+    }
+
+
     return (
-        <div className='edit-customer'>
+        <div className='edit-customer' autoComplete='off'>
             <h2>Edit Customer Info</h2>
             <hr />
             <div>
-                <form className='form-customer'>
+                <form className='form-customer' >
                     <div>
-                        <label htmlFor="firstName">First Name</label>
-                        <input type="text" name='firstName' />
+                        <label htmlFor="first_name">First Name</label>
+                        <span className='input' contenteditable="true" name='first_name'>{cus ? cus.first_name : ''}</span>
                     </div>
                     <br />
                     <div>
-                        <label htmlFor="lastName">Last Name</label>
-                        <input type="text" name='lastName' />
+                        <label htmlFor="last_name">Last Name</label>
+                        <span className='input' contenteditable="true" name='last_name'>{cus ? cus.last_name : ''}</span>
                     </div>
                     <br />
 
                     <div>
                         <label htmlFor="email">Email</label>
-                        <input type="text" name='email' />
+                        <span className='input' contenteditable="true" name='email'>{cus ? cus.email : ''}</span>
                     </div>
                     <br />
 
                     <div>
                         <label htmlFor="phone">Phone</label>
-                        <input type="text" name='phone' />
+                        <span className='input' contenteditable="true" name='phone'>{cus ? cus.phone : ''}</span>
                     </div>
                     <br />
                     
                     <div>
                         <label htmlFor="address">Address</label>
-                        <input type="text" name='address' />
+                        <span className='input' contenteditable="true" name='address'>{cus ? cus.address : ''}</span>
                     </div>
                     <br />
                     
                     <div>
                         <label htmlFor="district">District</label>
-                        <input type="text" name='district' />
+                        <span className='input' contenteditable="true" name='district'>{cus ? cus.district : ''}</span>
                     </div>
                     <br />
                    
                    <div>
                         <label htmlFor="city">City</label>
-                        <input type="text" name='city' />
+                        <span className='input' contenteditable="true" name='city'>{cus ? cus.city : ''}</span>
                    </div>
                     <br />
                    
                    <div>
                         <label htmlFor="postalcode">Postal Code</label>
-                        <input type="text" name='postalcode' />
+                        <span className='input' contenteditable="true" name='postal_code'>{cus ? cus.postal_code : ''}</span>
                    </div>
+                   <br />
+                   <br />
+                   <button onClick={(e) => submitHandler(e)} >Submit Changes</button>
                 </form>
             </div>
         </div>
